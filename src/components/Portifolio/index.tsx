37614b0fc43c0d3c5125516a1c38524663fa0c40
njauprince@gmail.com <njauprince@gmail.com>
2023-12-02 06:26:18 +0300

    filter by type

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -1,4 +1,3 @@
-
 import SingleRide from "./SingleRide";
 import vihicles, { CarProps } from "../../services/allVihicles";
 import { useState } from "react";
@@ -51,7 +50,6 @@ const Portifolio: React.FC = () => {
       brand_name: "Coupe",
       image: coupe,
     },
-   
   ];
 
   const handleBrandClick = (brand_name: string) => {
@@ -63,18 +61,15 @@ const Portifolio: React.FC = () => {
   };
 
   return (
-    <section className=" relative  my-24 max-w-6xl lg:mx-auto mx-4 flex items-center justify-center overflow-hidden">
+    <section className=" relative  my-24 max-w-6xl lg:mx-auto px-4 flex items-center justify-center overflow-hidden">
       <div className="flex flex-col items-center justify-center gap-4   w-full mx-auto  ">
         <p className="text-center font-bold text-primary">Choose Your Ride</p>
         <h2 className="text-3xl text-gray-950 font-bold g:text-5xl">
           Explore Our Rides By Type
         </h2>
 
-    
-
-  
         <div
-          className="grid grid-flow-col grid-rows-2 pb-3 overflow-x-auto overflow-y-hidden  py-4   lg:w-[90%]  w-96  md:grid-rows-1 gap-x-4 gap-y-8
+          className="grid grid-flow-col grid-rows-2 pb-3 overflow-x-auto overflow-y-hidden  py-4   lg:w-[90%]  w-full  md:grid-rows-1 gap-x-4 gap-y-8
           hover:scrollbar-thumb-primary/80 scrollbar-thumb-rounded-lg scrollbar-track-red-50  scrollbar-thumb-primary 
         scrollbar-thin"
         >
@@ -83,17 +78,17 @@ const Portifolio: React.FC = () => {
               <a
                 key={index}
                 onClick={() => handleBrandClick(brand.brand_name)}
-                className="flex pb-4 w-44 flex-col h-40  items-center duration-200 ease-in-out border-[0.5px] hover:border-[1px] border-gray-300 rounded-md cursor-pointer hover:scale-105 "
+                className="flex pb-4 w-44 flex-col h-36  items-center duration-200 ease-in-out border-[0.5px] hover:border-[1px] border-gray-300 rounded-md cursor-pointer hover:scale-105 "
               >
                 <img
                   alt={brand.brand_name}
                   loading="lazy"
-                  width="266"
-                  height="240"
+                  width="150"
+                  height="150"
                   decoding="async"
                   data-nimg="1"
-                  className="w-fit"
-                  src={suv}
+                  className="w-"
+                  src={brand.image}
                 ></img>
                 <span className="relative font-semibold capitalize -top-8 text-motokaa-primary">
                   {brand.brand_name}
